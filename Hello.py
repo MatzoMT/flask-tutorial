5c59b7fbfb78f3036ce5a1d2df3ec6639616b2f8
Matthew Tzou <sonic55099@gmail.com>
2021-06-03 19:31:47 -0400

    flask - variable rules completed

--- Hello.py
+++ Hello.py
@@ -11,7 +11,8 @@ def hello_world():
 def hello_deutsch():
     return 'Hallo Welt'
 
-@app.route('/suomi')
+# suomi will route correctly with/wo trailing slash
+@app.route('/suomi/')
 def hello_suomi():
     return 'Hei maailma'
 
@@ -23,6 +24,23 @@ app.add_url_rule('/espanol', 'espanol', hello_wada)
 def hello_world():
     return 'Hello world'
 app.add_url_rule('/', 'hello', hello_world)
+
+# Below routes anything not mentioned above to say 'Hey var'! Good for error handling?
+@app.route('/<var>')
+def hello_var(var):
+   return 'Hey %s!' % var
+
+@app.route('/goodbye/<var>')
+def goodbye_var(var):
+   return 'Goodbye %s!' % var
+
+# floatvar must be in floating point form (having a decimal point)
+@app.route('/float/<float:floatvar>')
+def show_float(floatvar):
+    return 'Float is %f' % floatvar
+
+if __name__ == '__main__':
+   app.run(debug = True)
 
 if __name__ == '__main__':
    app.run(debug = True)
